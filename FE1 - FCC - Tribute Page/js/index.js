63b2534c3a186cdb9f48d5b2fa7156a1e969ea08
Sam Atkinson <Sam Atkinson>
2016-11-29 16:12:59 +0000

    Initial Commit

--- FE1 - FCC - Tribute Page/js/index.js
+++
@@ -1,6 +0,0 @@
-// $(document).ready(function(){
-  //$(".infobox").hover(function(){
-   // $(this).toggleClass("test");
-    // $(this).find(".infotext").toggleClass("infotexta")
- //  });
-  // });
